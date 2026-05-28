9de823b181d576c42177566cf0c87cc766c3ed8f
Josué Barrios Ramírez <130526195+JosBar765@users.noreply.github.com>
2026-05-27 18:16:40 -0600

    agregando scripts con los esquemas de la tabla Empleados para ambas bases de datos

---
+++ database-scripts/MySQL.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `Empleado` (
+  `dpi` varchar(13) PRIMARY KEY NOT NULL,
+  `primer_nombre` varchar(30),
+  `segundo_nombre` varchar(30),
+  `primer_apellido` varchar(30),
+  `segundo_apellido` varchar(30),
+  `direccion` text,
+  `telefono_casa` varchar(8) UNIQUE,
+  `telefono_movil` varchar(8) UNIQUE,
+  `salario_base` decimal(10,2),
+  `bonificacion` int,
+
+  `fecha_modificacion` datetime,
+  `eliminado` boolean
+);
